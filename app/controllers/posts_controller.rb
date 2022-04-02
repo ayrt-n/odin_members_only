3c6c1849e1d0e3eff3b8145e30718ad62ddf70cc
ayrton <ayrton.parkinson1@gmail.com>
2022-04-02 15:02:36 -0400

    Fix issue with #create parameters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,12 +9,18 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = current_user.post.build(title: params[:title], body: params[:body])
+    @post = current_user.posts.build(post_params)
 
     if @post.save
       redirect_to posts_url
     else
       render :new
     end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
